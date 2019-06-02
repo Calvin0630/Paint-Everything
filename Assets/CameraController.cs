cc9012e371d86e67ecd738c95b37420a4f3a77f1
Calvin0630 <cg7057705@gmail.com>
2019-06-02 17:32:30 -0400

    first

---
+++ Assets/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public GameObject[] targets;
+    public Vector3 offset;
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    void Update() {
+        Vector3 targetAverage = Vector3.zero;
+        for (int i = 0; i < targets.Length; i++) {
+            targetAverage += targets[i].transform.position;
+        }
+        targetAverage /= targets.Length;
+        transform.position = targetAverage + offset;
+        transform.LookAt(targetAverage);
+    }
+}
