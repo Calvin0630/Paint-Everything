772c80e803fd0f9900e00843df433680f89304d3
Zedecous <swatenya@gmail.com>
2019-06-22 17:46:17 -0400

    Added jumping and boost mechanic

--- Assets/Resources/Scripts/PlayerController.cs
+++ Assets/Resources/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
     public float ballSpeed;
+    public float boost;
     //an int that represents the player number [1-4]
     public int playerID;
     Rigidbody body;
@@ -12,6 +13,7 @@ public class PlayerController : MonoBehaviour {
 		if (ballSpeed==0) Debug.Log("ballSpeed is 0!!!!");
         if (playerID < 1 || playerID > 4) Debug.Log("invalid playerID. got: " + playerID + ". expected a value from 1-4");
         body = GetComponent<Rigidbody>();
+        boost = 1f;
     }
 	
 	// Update is called once per frame
@@ -20,9 +22,23 @@ public class PlayerController : MonoBehaviour {
         if (isOnGround()) {
             float xSpeed = Input.GetAxis("Horizontal "+playerID);
             float ySpeed = Input.GetAxis("Vertical " +playerID);
+            //check if player is pressing the boost button, if it is, add boost, otherwise no boost
+            if (Input.GetButton("Boost " + playerID)) {
+                Debug.Log("Boosting");
+                boost = 1.5f;
+            }
+            else {
+                boost = 1f;
+            }
+            //Debug.Log(boost);
             //body.AddTorque(new Vector3(xSpeed, 0, ySpeed) * ballSpeed * Time.deltaTime);
-            body.AddForce(new Vector3(xSpeed, 0, ySpeed) * ballSpeed);
+            body.AddForce(new Vector3(xSpeed, 0, ySpeed) * ballSpeed * boost);
+            //check if player is pressing the jump button
+            if (Input.GetButtonDown("Jump " + playerID)) {
+                jump();
+            }
         }
+        
     }
 
     bool isOnGround() {
@@ -35,5 +51,11 @@ public class PlayerController : MonoBehaviour {
             }
         }
         return result;
+    }
+
+    //Player jumping mechanic. Has to be on the ground to be ran from update
+    void jump() {
+        float ySpeed = 350f;
+        body.AddForce(new Vector3(0, ySpeed, 0));
     }
 }
