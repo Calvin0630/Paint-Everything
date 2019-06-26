baa9b16818d4e7c464799d3ef9759312e4a95027
Calvin0630 <cg7057705@gmail.com>
2019-06-26 18:18:36 -0400

    added a gameover clock

---
+++ Assets/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    //hom long the round will be in seconds
+    public int roundDuration;
+    public Text clock;
+    // Start is called before the first frame update
+    void Start() {
+        StartCoroutine(UpdateClock());
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+    IEnumerator UpdateClock() {
+        while (roundDuration>0) {
+            //formating the time in second into a digital clock string
+            int minutesInt = roundDuration / 60;
+            int secondsInt = roundDuration - 60 * minutesInt;
+            string minutesString = "";
+            if (minutesInt < 10) minutesString = "0" + minutesInt;
+            else minutesString = ""+minutesInt;
+            string secondsString = "";
+            if (secondsInt < 10) secondsString = "0" + secondsInt;
+            else secondsString = "" + secondsInt;
+            clock.text = minutesString + ":" + secondsString;
+            roundDuration--;
+            yield return new WaitForSeconds(1);
+        }
+        clock.text = "GAMEOVER";
+        UpdateClock();
+    }
+}
